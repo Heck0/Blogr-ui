d5ec02397fdcc8d64f73eae55ec1b811875b52c7
Alexandre <alexandresavardplante@gmail.com>
2021-04-27 14:45:05 -0400

    Animations menu

--- script.js
+++ script.js
@@ -12,27 +12,11 @@ document.querySelectorAll(".navigation__list--item").forEach((list) => {
 hamburgerHandler.addEventListener("click", handleMenu);
 
 function handleMenuItem(e) {
-  let arrow = window.getComputedStyle(this, ":after");
-  this.nextElementSibling.classList.toggle("visually-hidden");
+  const arrow = this.nextElementSibling;
+  const menuItems = e.target.parentNode.lastElementChild;
 
-  console.log(arrow.getPropertyValue("width"));
-  arrow.style.transform = "rotate(180deg)";
-  // const target = e.currentTarget.lastElementChild;
-  // // const arrow = e.currentTarget;
-  // if (target.classList.contains("visually-hidden")) {
-  //   e.target.children[0].style.transform = "rotate(180deg)";
-  //   target.classList.remove("visually-hidden");
-  //   target.classList.add("fade-in");
-  // } else {
-  //   target.classList.add("fade-out");
-
-  //   setTimeout(() => {
-  //     target.classList.remove("fade-out");
-  //     target.classList.remove("fade-in");
-  //     target.classList.add("visually-hidden");
-  //     e.target.children[0].style.transform = "rotate(0deg)";
-  //   }, FADE_DELAY);
-  // }
+  arrow.classList.toggle("animationRotate");
+  menuItems.classList.toggle("visually-hidden");
 }
 
 function handleMenu() {
